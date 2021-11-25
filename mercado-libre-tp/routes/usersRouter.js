044c2e8e6aceeff51251add1a91771fdbb05958e
marilyn <vinomarilyn@gmail.com>
2021-11-25 17:31:22 -0300

    solucionando la ruta login

--- mercado-libre-tp/routes/usersRouter.js
+++ mercado-libre-tp/routes/usersRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 let usersController = require('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/:login', usersController.login);//metodo que devuelve la vista
-router.get('/:register', usersController.register);
+router.get('/login', usersController.login);//metodo que devuelve la vista
+router.get('/register', usersController.register);
 
 module.exports = router;
